c203a9e96f4513ddeec79c926f3c4a1af1c29a70
Sophia!!! :)  🐺✌ <sdoyle_msasd@magnoliascience.org>
2026-09-23 18:25:03 +0000

    Fix double jump input and increase speed

--- js/input.js
+++ js/input.js
@@ -34,7 +34,7 @@ window.addEventListener("keyup", function (event) {
 function setKey(key, isDown) {
   if (key === "ArrowLeft"  || key === "a" || key === "A") { Input.left  = isDown; }
   if (key === "ArrowRight" || key === "d" || key === "D") { Input.right = isDown; }
-  if (key === "ArrowUp"    || key === "w" || key === "W") {
+  if (key === "ArrowUp"    || key === "w" || key === "W" || key === " ") {
     if (isDown && !Input.jump) { Input.jumpPressed = true; }
     Input.jump = isDown;
   }
